23554adb07ac1d595c25a58e60ed648c9bdfd14a
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-04-29 13:43:24 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
 
+function onFormSubmit(event) {
+    event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+  alert('Всі поля повинні бути заповнені!');  
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
+
+form.addEventListener("submit", onFormSubmit);
