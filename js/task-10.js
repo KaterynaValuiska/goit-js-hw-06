f1e3a73e9aaea859891a988b0e1928d1750af310
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-04-30 22:46:56 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+const input = document.querySelector('input');
+
+
+
+function createBoxes(amount) {
+  amount = input.value;
+  for (let i = 0; i < amount; i += 1) {
+    let box = document.createElement('div');
+    box.style.width = i * 10 + 30 + 'px';
+    box.style.height = i*10 + 30 + 'px';
+    box.style.backgroundColor = getRandomHexColor();
+    console.log(box); 
+    boxes.before(box);
+  } 
+}
+
+btnCreate.addEventListener("click", createBoxes);
+
+
+btnDestroy.addEventListener("click", () => {
+  
+  btnDestroy.removeEventListener("click", createBoxes);
+  box.remove();
+});
+
+
+
+
