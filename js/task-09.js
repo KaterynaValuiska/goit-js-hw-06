3e54d38d40fcba0c333d6e03bba0e41c9dab3cb3
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-05-02 15:13:50 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -12,7 +12,5 @@ btnColor.addEventListener("click", onChangeColor);
 
 function onChangeColor(event) {
   body.style.backgroundColor = getRandomHexColor();
-  spanColor.textContent = `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  spanColor.textContent = body.style.backgroundColor;
 }
